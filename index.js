aa060d0758867c07cd6647040385195c84fc0e25
Afanasii Kurakin <afanasy@gmail.com>
2021-03-18 12:24:43 +0300

    added message check

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ module.exports = () => {
     [ln, _.filter(_.map(fs.readFileSync(__dirname + '/../.' + package.name + '/' + ln, 'utf8').split('\n'), d => d.trim(), _.identity))]
   ))
   return (req, res) => {
+    console.log(req.body)
+    if (!req.body.message)
+      return res.end()
     var ln = 'en'
     if (req.body.message && /[а-яА-Я]/.test(req.body.message.text))
       ln = 'ru'
